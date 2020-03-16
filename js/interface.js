94a9d75666d53984415db11842a87dabde8d1b9f
lazlo barragan <lazlo.barragan@gmail.com>
2020-03-16 01:15:58 +0100

    color picker

--- js/interface.js
+++ js/interface.js
@@ -27,3 +27,66 @@ function onclickIcon(e) {
     this.classList.remove("-clear-logo");
     this.classList.add("-dark-logo");
 }
+const picker = new iro.ColorPicker('#picker', {
+    width: 160,
+    color: "rgb(255, 0, 0)",
+    borderWidth: 1,
+    borderColor: "#fff",
+    layout: [{
+            component: iro.ui.Box,
+        },
+        {
+            component: iro.ui.Slider,
+            options: {
+                sliderType: 'hue'
+            }
+        },
+        {
+            component: iro.ui.Slider,
+            options: {
+                sliderType: 'alpha'
+            }
+        }
+    ]
+});
+const picker2 = Pickr.create({
+    el: '.pickr',
+    theme: 'classic',
+
+    swatches: [
+        'rgba(244, 67, 54, 1)',
+        'rgba(233, 30, 99, 0.95)',
+        'rgba(156, 39, 176, 0.9)',
+        'rgba(103, 58, 183, 0.85)',
+        'rgba(63, 81, 181, 0.8)',
+        'rgba(33, 150, 243, 0.75)',
+        'rgba(3, 169, 244, 0.7)',
+        'rgba(0, 188, 212, 0.7)',
+        'rgba(0, 150, 136, 0.75)',
+        'rgba(76, 175, 80, 0.8)',
+        'rgba(139, 195, 74, 0.85)',
+        'rgba(205, 220, 57, 0.9)',
+        'rgba(255, 235, 59, 0.95)',
+        'rgba(255, 193, 7, 1)'
+    ],
+
+    components: {
+
+        // Main components
+        preview: true,
+        opacity: true,
+        hue: true,
+
+        // Input / output Options
+        interaction: {
+            hex: true,
+            rgba: true,
+            hsla: true,
+            hsva: true,
+            cmyk: true,
+            input: true,
+            clear: true,
+            save: true
+        }
+    }
+});
